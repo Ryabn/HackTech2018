4d19ad9d59c6908cdf56af39cb21b8ec741014be
Ryan Yang <javathon@gmail.com>
2018-03-10 16:24:49 -0800

    collision box coordinates are done, now writing the code to check if point is inside it'

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ function analyzePositions(playerData, clientId){
     if(canShootGameData[clientId] && playerData['shoot']){
         gameData[clientId]['shoot'] = true;
         canShootGameData[clientId] = false;
-        //checkCollision(playerData['x'],playerData['y'],playerData['angle'];);
+        checkCollision(playerData['x'], playerData['y'], playerData['angle']);
         setTimeout(function(){
             gameData[clientId]['shoot'] = false;
             setTimeout(function(){
@@ -73,7 +73,7 @@ function checkCollision(x, y, angle){
     //-tan of angle is slope of the line
     // b = y - (tan(angle) * x)
     // m = tan(angle)
-    // 
+    angle = -angle + 0.075;
     //point 1:
     var x1 = x + 20 * Math.sin(angle);
     var y1 = y + 20 * Math.cos(angle);
@@ -81,12 +81,42 @@ function checkCollision(x, y, angle){
     var x2 = x + x - x1;
     var y2 = y + y - y1;
     //point 3:
-    var x3 = x1 + 70 * Math.sin(angle + 1.5708);
-    var y3 = y1 + 70 * Math.cos(angle + 1.5708);
+    var x3 = x1 + 110 * Math.sin(angle + 1.5708);
+    var y3 = y1 + 110 * Math.cos(angle + 1.5708);
     //point 4:
     var x4 = x3 + (x2 - x1);
     var y4 = y3 + (y2 - y1);
+    
+    gameData['collisionbox'] = {
+        'p1': [x1, y1],
+        'p2': [x2, y2],
+        'p3': [x3, y3],
+        'p4': [x4, y4]
+    };
+    
+    var xSorted = {
+    }
+    var ySort = [y1, y2, y3, y4];
+    var xSort = [x1, x2, x3, x4];
+    
+    for(i = 0; i < 4; i++){
+        xSorted[ySort[i]] = xSort[i];
+    }
+    ySort.sort();
+
     for(var key in gameData){
+        var pX = gameData[key]['x'];
+        var pY = gameData[key]['y'];
+        if((pY <= ySort[0]) && (pY >= ySort[3])){
+            if(pY <= ySort[1]){
+                //between 0 and 1
+            }else if(pY <= ySort[2]){
+                //between 1 and 2
+                
+            }else{
+                //between 2 and 3
+            }
+        }
         
     }
 }
